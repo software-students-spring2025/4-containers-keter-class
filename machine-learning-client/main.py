fb9f64e8bfb8dffab6c9d98ebc130279948157ef
AnnaTheYe <azy2006@nyu.edu>
2025-04-06 21:34:26 -0400

    fixing linting issues and adding card regex and mongo

---
+++ machine-learning-client/main.py
@@ -0,0 +1,54 @@
+"""
+Machine learning API and parsing and storage of credit card information
+"""
+import re
+from web_app.app import mongo
+
+TEST_CARD_SCAN = (
+    "realawesomebank.com 12345 AB 03/20 123456ABC "
+    "1234 4321 1010 5454 Valid Thru: 01/26 Sec code: 123 "
+    "JOHN JACOB JINGLEHEIMER SCHMIDT US and Canada 800.421.211 "
+    "Int'l: 302-594-8200 Made from recycled plastic"
+)
+TEST_USERNAME = "j3schmidt"
+TEST_CARDNAME = "realawesomebank card 1"
+
+
+def add_card_info(card_scan, username, cardname):
+    """
+    Breaks down text scan into card information and saves to MongoDB
+
+    Parameters:
+    card_scan (str): text from ML image scan as a single line string
+    username (str): website user identification (could be replaced with user_id or the likes)
+    cardname (str): user chosen name for identifying multiple cards per user
+    """
+    cardholder_name = re.findall(r"\b(?:[A-Z]+\.?\s+)+[A-Z]+\.?(?<!\bUS)\b", card_scan)
+    assert len(cardholder_name) == 1, f"{len(cardholder_name)} card holders found"
+    card_number = re.findall(r"\d{4} \d{4} \d{4} \d{4}", card_scan)
+    assert len(card_number) == 1, f"{len(card_number)} card numbers found"
+    cvv = re.findall(r"(?<![.-])\b\d{3}\b(?![.-]\d)", card_scan)
+    assert len(cvv) == 1, f"{len(cvv)} cvvs found"
+    # there's a small little date at the top of the card, not sure what it is
+    expiry_date = re.findall(r"\d{2}\/\d{2}", card_scan)
+    assert len(expiry_date) >= 1, "no expiry date found"
+    cardholder_name = cardholder_name[0]
+    card_number = card_number[0]
+    cvv = cvv[0]
+    expiry_date = expiry_date[-1]
+
+    mongo.db.cards.insert_one(
+        {
+            "user": username,
+            "card_name": cardname,
+            "card_holder": cardholder_name,
+            "card_number": card_number,
+            "cvv": cvv,
+            "expiry_date": expiry_date,
+        }
+    )
+
+    return cardholder_name, card_number, cvv, expiry_date, username, cardname
+
+
+add_card_info(TEST_CARD_SCAN, TEST_USERNAME, TEST_CARDNAME)
--- web_app/run.py
+++ web_app/run.py
@@ -1,7 +1,9 @@
+"""
+Running app
+"""
 from app import create_app
 
-app = create_app()
+app, db = create_app()
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5001)
-
