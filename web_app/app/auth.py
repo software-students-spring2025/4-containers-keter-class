9cf2d27bb4eb8bf4824623186b6a110dfb905ad1
AnnaTheYe <azy2006@nyu.edu>
2025-04-06 21:47:40 -0400

    fixing web app linting

--- web_app/app/auth.py
+++ web_app/app/auth.py
@@ -1,11 +1,19 @@
+"""
+Login
+"""
 from flask import Blueprint, render_template, request, redirect, url_for, session, flash
 from werkzeug.security import generate_password_hash, check_password_hash
-from . import mongo
 
 auth = Blueprint("auth", __name__)
 
+
 @auth.route("/login", methods=["GET", "POST"])
 def login():
+    """
+    login
+    """
+    from app import mongo  # pylint: disable=import-outside-toplevel
+
     if request.method == "POST":
         user = mongo.db.users.find_one({"email": request.form["email"]})
         if user and check_password_hash(user["password"], request.form["password"]):
@@ -14,23 +22,32 @@ def login():
         flash("Invalid credentials")
     return render_template("login.html")
 
+
 @auth.route("/register", methods=["GET", "POST"])
 def register():
+    """
+    register
+    """
+    from app import mongo  # pylint: disable=import-outside-toplevel
+
     if request.method == "POST":
         existing = mongo.db.users.find_one({"email": request.form["email"]})
         if existing:
             flash("Email already registered.")
         else:
             hashed_pw = generate_password_hash(request.form["password"])
-            mongo.db.users.insert_one({
-                "email": request.form["email"],
-                "password": hashed_pw
-            })
+            mongo.db.users.insert_one(
+                {"email": request.form["email"], "password": hashed_pw}
+            )
             flash("Registered successfully. Please log in.")
             return redirect(url_for("auth.login"))
     return render_template("register.html")
 
+
 @auth.route("/logout")
 def logout():
+    """
+    logout
+    """
     session.clear()
     return redirect(url_for("auth.login"))
