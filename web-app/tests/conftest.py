3b91a7272baaa205facf545679a5f741229efbe9
bryantto08 <bryantto08@gmail.com>
2025-04-08 15:25:04 -0400

    fixed conftest errors again

--- web-app/tests/conftest.py
+++ web-app/tests/conftest.py
@@ -19,18 +19,18 @@ def app():
     os.environ["MONGO_URI"] = "mongodb://localhost:27017/test_flaskdb"
     os.environ["SECRET_KEY"] = "testing"
 
-    app1 = create_app()
+    app = create_app()
 
     # Drop users collection before each test for a clean slate
-    with app1.app_context():
-        app1.db.users.delete_many({})
+    with app.app_context():
+        app.db.users.delete_many({})
 
-    return app1
+    return app
 
 
 @pytest.fixture
-def client(app1):
+def client(app):
     """
     idk
     """
-    return app1.test_client()
+    return app.test_client()
