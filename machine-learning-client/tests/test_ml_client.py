919364b85ee7749c98d5ee35f81d9e1c86d98c0e
AnnaTheYe <azy2006@nyu.edu>
2025-04-09 16:48:22 -0400

    restoring test_ml_client

--- machine-learning-client/tests/test_ml_client.py
+++ machine-learning-client/tests/test_ml_client.py
@@ -2,64 +2,10 @@
 Testing for ML Client
 """
 
-import pytest
-import main
-from mongomock import MongoClient
 
-
-class Tests:
+def test_hello_world():
     """
-    tests
+    Testing hello world
     """
-
-    @pytest.fixture
-    def mock_db(self):
-        """
-        Mock db for testing
-        """
-        client = MongoClient()
-        db = client.test_flaskdb
-        yield db
-        client.close()
-
-    @pytest.fixture
-    def client(self):
-        """
-        idk
-        """
-        return
-
-    def parse_card_info_invalid_card(self, mock_db):
-        """
-        Testing invalid card inputs
-        """
-
-        test_card_scan_incorrect_num = (
-            "realawesomebank.com 12345 AB 03/20 123456ABC "
-            "1234 4321 1010 54546 Valid Thru: 01/26 Sec code: 123 "
-            "JOHN JACOB JINGLEHEIMER SCHMIDT US and Canada 800.421.211 "
-            "Int'l: 302-594-8200 Made from recycled plastic"
-        )
-        test_username = "j3schmidt"
-        test_cardname = "realawesomebank card 1"
-
-        test_card_scan_incorrect_name = (
-            "realawesomebank.com 12345 AB 03/20 123456ABC "
-            "1234 4321 1010 5454 Valid Thru: 01/26 Sec code: 123 "
-            "JOHN US and Canada 800.421.211 "
-            "Int'l: 302-594-8200 Made from recycled plastic"
-        )
-
-        test_card_scan_incorrect_expiry = (
-            "realawesomebank.com 12345 AB 03/20 123456ABC "
-            "1234 4321 1010 5454 Valid Thru: 01/26 Sec code: 123 "
-            "JOHN JACOB JINGLEHEIMER SCHMIDT US and Canada 800.421.211 "
-            "Int'l: 302-594-8200 Made from recycled plastic"
-        )
-
-        test_card_scan_incorrect_cvv = (
-            "realawesomebank.com 12345 AB 03/20 123456ABC "
-            "1234 4321 1010 5454 Valid Thru: 01/26 Sec code: 12345 "
-            "JOHN JACOB JINGLEHEIMER SCHMIDT US and Canada 800.421.211 "
-            "Int'l: 302-594-8200 Made from recycled plastic"
-        )
+    string = "Hello World"
+    assert "Hello World" == string
